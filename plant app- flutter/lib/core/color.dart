fdc32d07fdb39c6478d7fdf14a4858b472df6fd8
Kolla om vitesh <56569491+vitesh12@users.noreply.github.com>
2022-04-22 20:24:34 +0530

    app

---
+++ plant app- flutter/lib/core/color.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+const Color green = Color(0xFF2FA849);
+const Color white = Color(0xFFFFFFFF);
+const Color black = Color(0xFF071B0C);
+const Color blue = Colors.blue;
+
+const Color lightGreen = Color(0xFFECF4F3);
+const Color grey = Color(0xFF9C9C9C);
+const Color lightGrey = Color(0xFFEBEBEB);
---
+++ plant app- flutter/lib/data/plant_data.dart
@@ -0,0 +1,77 @@
+import 'package:ui_13/data/plant_model.dart';
+
+List<Plants> plants = [
+  Plants(
+    id: 0,
+    name: 'Broken Heart Plant',
+    imagePath: 'assets/images/plant1.png',
+    category: 'Office',
+    description:
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Quis ipsum suspendisse ultrices gravida. Risus commodo viverra maecenas accumsan lacus vel facilisis. ',
+    price: 75.00,
+    isFavorit: false,
+  ),
+  Plants(
+    id: 1,
+    name: 'Money Plant Golden',
+    imagePath: 'assets/images/plant2.png',
+    category: 'Office',
+    description:
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Quis ipsum suspendisse ultrices gravida. Risus commodo viverra maecenas accumsan lacus vel facilisis. ',
+    price: 75.00,
+    isFavorit: false,
+  ),
+  Plants(
+    id: 2,
+    name: 'Echeveria Pegasus',
+    imagePath: 'assets/images/plant3.png',
+    category: 'Office',
+    description:
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Quis ipsum suspendisse ultrices gravida. Risus commodo viverra maecenas accumsan lacus vel facilisis. ',
+    price: 60.00,
+    isFavorit: false,
+  ),
+  Plants(
+    id: 3,
+    name: 'Wandering Jew Plant',
+    imagePath: 'assets/images/plant4.png',
+    category: 'Office',
+    description:
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Quis ipsum suspendisse ultrices gravida. Risus commodo viverra maecenas accumsan lacus vel facilisis. ',
+    price: 90.00,
+    isFavorit: false,
+  ),
+];
+
+List<Plants> populerPlants = [
+  Plants(
+    id: 0,
+    name: 'Golden Hahnii',
+    imagePath: 'assets/images/plant5.png',
+    category: 'Office',
+    description:
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Quis ipsum suspendisse ultrices gravida. Risus commodo viverra maecenas accumsan lacus vel facilisis. ',
+    price: 75.00,
+    isFavorit: false,
+  ),
+  Plants(
+    id: 1,
+    name: 'Money Tree',
+    imagePath: 'assets/images/plant6.png',
+    category: 'Office',
+    description:
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Quis ipsum suspendisse ultrices gravida. Risus commodo viverra maecenas accumsan lacus vel facilisis. ',
+    price: 75.00,
+    isFavorit: false,
+  ),
+  Plants(
+    id: 2,
+    name: 'Chinese Money Plant',
+    imagePath: 'assets/images/plant7.png',
+    category: 'Office',
+    description:
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Quis ipsum suspendisse ultrices gravida. Risus commodo viverra maecenas accumsan lacus vel facilisis. ',
+    price: 60.00,
+    isFavorit: false,
+  ),
+];
