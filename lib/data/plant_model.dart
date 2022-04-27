de79a8af5ec843e323df8448208bd260b5f6f6c4
Kolla om vitesh <56569491+vitesh12@users.noreply.github.com>
2022-04-27 14:37:16 +0530

    part 5

---
+++ lib/data/plant_model.dart
@@ -0,0 +1,20 @@
+class Plants{
+  final int id;
+  final String name;
+  final String imagePath;
+  final String category;
+  final String description;
+  final double price;
+  final bool isFavourit;
+
+  
+Plants({
+  required this.id,
+  required this.name,
+  required this.imagePath,
+  required this.category,
+  required this.description,
+  required this.price,
+  required this.isFavourit,
+});
+}
